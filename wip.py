1cb9164e2545ab4ee9bb070f475b4aaeb44f9108
eloquentarduino <eloquentarduino@gmail.com>
2022-01-29 17:46:12 +0100

    fixed bug in logistic regression porting (no intercept)

--- wip.py
+++ wip.py
@@ -1,13 +1,16 @@
-from sklearn.datasets import load_boston
+from sklearn.datasets import *
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LogisticRegression
 from micromlgen import port, port_testset
 
 
 if __name__ == '__main__':
     X, y = load_boston(return_X_y=True)
+    X, y = load_iris(return_X_y=True)
     #regr = DecisionTreeRegressor(max_depth=10, min_samples_leaf=5).fit(X, y)
-    regr = RandomForestRegressor(n_estimators=2, max_depth=10, min_samples_leaf=5).fit(X, y)
+    #regr = RandomForestRegressor(n_estimators=2, max_depth=10, min_samples_leaf=5).fit(X, y)
+    regr = LogisticRegression(max_iter=100).fit(X, y)
     y_pred = regr.predict(X)
 
     with open('wip.txt', 'w') as file:
